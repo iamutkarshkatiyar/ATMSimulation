848f56141669eeeecad48fbff30c5d9c6570c8cd
iamutkarshkatiyar <utkarshkatiyar2004@gmail.com>
2026-06-26 22:37:25 +0530

    menu added

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,35 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    IO.println(String.format("Hello and welcome!"));
+import java.util.Scanner;
 
-    for (int i = 1; i <= 5; i++) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        IO.println("i = " + i);
+class main{
+    static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int choice;
+        do {
+            System.out.println("1.Cash Withdrawal");
+            System.out.println("2.Cash Deposit");
+            System.out.println("3.Balance Enquiry");
+            System.out.println("4.Pin Update/Create");
+            System.out.println("5.Exit");
+            choice =sc.nextInt();
+            switch (choice){
+                case 1:
+                    System.out.println("cash withdrawal");
+                    break;
+                case 2:
+                    System.out.println("cash withdrawal");
+                    break;
+                case 3:
+                    System.out.println("cash withdrawal");
+                    break;
+                case 4:
+                    System.out.println("cash withdrawal");
+                    break;
+                case 5:
+                    System.out.println("cash withdrawal");
+                    break;
+                default:
+                    System.out.println("wrong choice");
+            }
+        }while(choice<=5);
     }
 }
